cf867b8ce54e6a5cc41abfe9099efc30fd269e1a
mida-hub <rusuden0106@gmail.com>
2021-01-17 14:55:00 +0900

    Adminモック

---
+++ front/src/views/Admin.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <el-row>
+      <Menu/>
+    </el-row>
+    <el-row>
+      <TagView/>
+    </el-row>
+  </div>
+</template>
+
+<script>
+import Menu from '@/components/admin/Menu'
+import TagView from '@/components/admin/TagView'
+
+export default {
+  components: {
+    Menu,
+    TagView
+  },
+}
+</script>
--- front/src/views/Home.vue
+++ front/src/views/Home.vue
@@ -1,33 +1,33 @@
 <template>
-<div>
-  <el-header>
-    <Header/>
-    <el-divider/>
-  </el-header>
-  <el-main>
-    <el-row style="width: 100%;">
-      <el-col>
-        <Tag/>
-      </el-col>
-    </el-row>
-    <el-row style="width: 80%;">
-      <el-col :offset="3">
-        <PostDetail/>
-      </el-col>
-    </el-row>
-  </el-main>
-  <el-footer>
-    <el-divider/>
-    <Footer/>
-  </el-footer>
-</div>
+  <div>
+    <el-header>
+      <Header/>
+      <el-divider/>
+    </el-header>
+    <el-main>
+      <el-row style="width: 100%;">
+        <el-col>
+          <Tag/>
+        </el-col>
+      </el-row>
+      <el-row style="width: 80%;">
+        <el-col :offset="3">
+          <PostDetail/>
+        </el-col>
+      </el-row>
+    </el-main>
+    <el-footer>
+      <el-divider/>
+      <Footer/>
+    </el-footer>
+  </div>
 </template>
 
 <script>
-import Header from '@/components/Header'
-import Footer from '@/components/Footer'
-import Tag from '@/components/Tag'
-import PostDetail from '@/components/PostDetail'
+import Header from '@/components/blog/Header'
+import Footer from '@/components/blog/Footer'
+import Tag from '@/components/blog/Tag'
+import PostDetail from '@/components/blog/PostDetail'
 
 export default {
   components: {
